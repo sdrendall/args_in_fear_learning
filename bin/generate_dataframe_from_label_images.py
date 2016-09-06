f6f60b6f44bc685b4742dd078ba9025f786ff5db
Sam Rendall <sdrendall@gmail.com>
2016-09-06 11:05:31 -0400

    Version of generate_dataframe_from_label_images used to generate data for big_experiment

--- bin/generate_dataframe_from_label_images.py
+++ bin/generate_dataframe_from_label_images.py
@@ -11,18 +11,21 @@ from fisherman import detection, math
 from argparse import ArgumentParser
 from skimage import io, measure
 from os import path, getcwd
+from itertools import ifilter
 
 VSI_PIXEL_SCALE = .64497 # in um/pixel
 
 def configure_parser():
     parser = ArgumentParser(description='Summarize a set of labelled images in a dataframe.')
-    parser.add_argument('label_dir', help='Path to the directory containing label files')
     parser.add_argument('output_path', help='Path where the output dataframe should be saved.')
+    parser.add_argument('image_paths', nargs='+', type=path.expanduser,
+                        help='Paths to the label files to generate dataframes from')
     parser.add_argument('-e', '--experiment_path', default=getcwd(),
                         help='Path to the vsi experiment root')
-    parser.add_argument('--flip', help='Corrects for vertically flipped vsi images')
-    parser.add_argument('--flop', help='Corrects for horizontally flipped vsi images')
-
+    parser.add_argument('--flip', action='store_true', default=False, 
+                        help='Corrects for vertically flipped vsi images')
+    parser.add_argument('--flop', action='store_true', default=False, 
+                        help='Corrects for horizontally flipped vsi images')
     return parser
 
 
@@ -46,11 +49,37 @@ def get_label_path(vsi_path, args):
     label_name = path.splitext(path.basename(vsi_path))[0] + '_fish_labels.tif'
     return path.join(args.label_dir, label_name)
 
+def get_corresponding_entry(label_path, metadata):
+    vsi_name = path.basename(label_path).replace('_fish_labels.tif', '.vsi')
+    for entry in metadata:
+        if vsi_name in entry['vsiPath']:
+            return entry
 
-def get_image_descriptor(entry, args):
-    label_path = get_label_path(entry['vsi_path'], args)
-    label_image = io.imread(label_path)
-    vsi_image = load_vsi(path.join(args.experiment_path, entry['vsi_path']))
+    return None
+
+def get_image_descriptor(label_path, metadata, args):
+    entry = get_corresponding_entry(label_path, metadata)
+    if entry is None:
+        print "Could not locate entry for label path:"
+        print label_path
+        print '--------------------------------------'
+        return None
+        
+    try:
+        label_image = io.imread(label_path)
+    except:
+        print "Could not load label image located at"
+        print label_path
+        print "--------------------------------------"
+        return None
+        
+    try:
+        vsi_image = load_vsi(path.join(args.experiment_path, entry['vsiPath']))
+    except:
+        print "Could not load vsi image:"
+        print path.join(args.experiment_path, entry['vsiPath'])
+        print "--------------------------------------"
+        return None
 
     vsi_res = numpy.asarray(vsi_image.shape[:2], dtype=numpy.int32)
     lab_res = numpy.asarray(label_image.shape[:2], dtype=numpy.int32)
@@ -59,19 +88,21 @@ def get_image_descriptor(entry, args):
     # The offset produced by the convolution window needs to be accounted for
     corrected_view = vsi_image[window_correction[0]:, window_correction[1]:, ...]
 
-    cells = [
-        Cell(
+    cells = (
+        detection.Cell(
             image=math.crop_from_bounding_box(props.bbox, corrected_view),
             mask=props.image,
             centroid=props.centroid,
             bounding_box=props.bbox
         ) for props in measure.regionprops(label_image)
-    ]
+    )
+
+    physical_cells = map(lambda cell: data.PhysicalCell.from_cell(cell, VSI_PIXEL_SCALE), cells)
 
     image_descriptor = data.ImageDescriptor.from_metadata(
         entry,
         experiment_path=args.experiment_path,
-        cells=cells,
+        cells=physical_cells,
         flip=args.flip,
         flop=args.flop
     )
@@ -95,8 +126,14 @@ def main():
     javabridge.start_vm(class_path=bioformats.JARS)
     log4j.basic_config()
 
-    image_descriptors = (get_image_descriptor(entry, args) for entry in metadata)
+    image_descriptors = ifilter(
+        lambda d: d is not None, 
+        (get_image_descriptor(label_path, metadata, args) for label_path in args.image_paths)
+    )
     df = dataframes.get_dataframe_from_image_sequence(image_descriptors)
     df.to_csv(args.output_path)
 
     javabridge.kill_vm()
+
+if __name__ == '__main__':
+    main()
